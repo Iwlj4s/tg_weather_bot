4e006be4cae840f4a9743fc3674d0736f046c7e3
Iwlj4s <elizavetalenn@mail.ru>
2023-09-08 15:42:46 +0300

    Bot working, but it still needs some work, adding checks, etc.

--- get_weather.py
+++ get_weather.py
@@ -1,2 +1,46 @@
 from dotenv import load_dotenv
+import datetime as dt
 import requests
+
+load_dotenv()
+
+
+def kelvin_to_celsius(kelvin):
+    celsius = kelvin - 273.15
+
+    return celsius
+
+
+def get_request_of_weather(city_name, token):
+    # Get Request #
+    BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+
+    url = BASE_URL + "appid=" + token + "&q=" + city_name
+    response = requests.get(url).json()
+
+    return response
+
+
+def get_response_of_weather(city_name, token):
+    weather_data = get_request_of_weather(city_name, token)
+
+    if weather_data:
+        weather_info = {
+            'description': weather_data['weather'][0]['description'],
+            'temperature': round(kelvin_to_celsius(weather_data['main']['temp'])),
+            'feels like': round(kelvin_to_celsius(weather_data['main']['feels_like'])),
+            'wind speed': round(weather_data['wind']['speed']),
+            'humidity': round(weather_data['main']['humidity'])
+        }
+
+        # --- Get Sin Rise And Sun Set --- #
+        sunrise_time = dt.datetime.utcfromtimestamp(weather_data['sys']['sunrise'] + weather_data['timezone'])
+        sunset_time = dt.datetime.utcfromtimestamp(weather_data['sys']['sunset'] + weather_data['timezone'])
+
+        weather_info['sun rise'] = sunrise_time
+        weather_info['sun set'] = sunset_time
+
+        return weather_info
+
+    else:
+        return None
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
 import os
 import get_weather
+
 load_dotenv()
 
 
@@ -18,9 +19,31 @@ class Handler(BotSettings):
     def __init__(self):
         super().__init__()
 
-        @self.dp.message_handler()
-        async def echo(message: types.Message):
-            await message.answer(message.text)
+        @self.dp.message_handler(commands=['start', 'help'])
+        async def send_welcome(message: types.Message):
+            await message.answer("Hi! I'm a weather bot. Enter your city name using only letters"
+                                 " and I'll tell you about the weather. Example: /weather Tokyo")
+
+        @self.dp.message_handler(commands=['weather'])
+        async def get_weather_info(message: types.Message):
+            city_name = message.text.split(' ', 1)[1]
+
+            weather_data = get_weather.get_response_of_weather(city_name, self.WEATHER_API)
+
+            if weather_data:
+                response_message = f"Weather in {city_name}:\n"
+                response_message += f"{weather_data['description']}\n"
+                response_message += f"Temperature: {weather_data['temperature']}(°C)\n"
+                response_message += f"Feels like: {weather_data['feels like']}(°C)\n"
+                response_message += f"Wind speed: {weather_data['wind speed']}(m/s)\n"
+                response_message += f"Humidity: {weather_data['humidity']}(%)\n"
+                response_message += f"Sunrise: {weather_data['sun rise']}\n"
+                response_message += f"Sunset: {weather_data['sun set']}"
+
+                await message.answer(response_message)
+
+            else:
+                await message.answer(f"Sorry, couldn't get weather information for {city_name}.")
 
 
 if __name__ == '__main__':
