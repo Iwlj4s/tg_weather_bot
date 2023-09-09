46da202f8a5f1bb932b2dd66336fb1d926c4071f
Iwlj4s <elizavetalenn@mail.ru>
2023-09-09 17:46:58 +0300

    Add checks for input and translation depending on the command(en to ru), I think it's final version. Mb I'll add something

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from aiogram import Bot, Dispatcher, executor, types
+from googletrans import Translator
 from dotenv import load_dotenv
+import traceback
 import os
 import get_weather
+from translate_dict import translate_text
 
 load_dotenv()
 
@@ -19,14 +22,16 @@ class Handler(BotSettings):
     def __init__(self):
         super().__init__()
 
+        self.translator = Translator()
+
         # --- Info Handler --- #
         @self.dp.message_handler(commands=['start', 'help'])
         async def send_welcome(message: types.Message):
             await message.answer("Hi! I'm a weather bot. Enter your city name using only letters"
-                                 " and I'll tell you about the weather. Example: /weather Tokyo")
+                                 " and I'll tell you about the weather. Example: /weather Tokyo or /погода Токио")
 
         # --- Main Handler --- #
-        @self.dp.message_handler(commands=['weather', 'погода'])
+        @self.dp.message_handler(lambda message: message.text.startswith(('/weather', '/погода')))
         async def get_weather_info(message: types.Message):
 
             try:
@@ -35,11 +40,11 @@ class Handler(BotSettings):
 
                 # --- Check if there at least two parts --- #
                 if len(user_message_parts) < 2:
-                    await message.answer("Please provide a valid city name after the /weather command.")
+                    await message.answer("Please provide a valid city name after the /weather or /погода command.")
                     return
 
                 # --- Get City Name And Weather Data --- #
-                city_name = user_message_parts[1]
+                city_name = user_message_parts[1].capitalize()
 
                 weather_data = get_weather.get_response_of_weather(city_name, self.WEATHER_API)
 
@@ -48,20 +53,28 @@ class Handler(BotSettings):
                     await message.answer(f"Sorry, couldn't get weather information for {city_name}.")
 
                 else:
+
+                    city_name_output = f"Weather in {city_name}:\n"
+
                     response_message = (
-                        f"Weather in {city_name}:\n"
                         f"{weather_data['description']}\n"
                         f"Temperature: {weather_data['temperature']}°C\n"
                         f"Feels like: {weather_data['feels like']}°C\n"
                         f"Wind speed: {weather_data['wind speed']}m/s\n"
                         f"Humidity: {weather_data['humidity']}%\n"
-                        f"Sunrise: {weather_data['sun rise']}\n"
-                        f"Sunset: {weather_data['sun set']}"
+                        f"SunRise: {weather_data['sun rise']}\n"
+                        f"SunSet: {weather_data['sun set']}"
                     )
 
-                    await message.answer(response_message)
+                    # --- Ru or En  --- #
+                    if message.text.startswith('/погода'):
+                        translated_message = translate_text(response_message, 'en', 'ru')
+                        await message.answer(city_name_output + translated_message)
+                    else:
+                        await message.answer(city_name_output + response_message)
 
             except Exception as e:
+                traceback.print_exc()
                 print(f"An error occurred: {e}")
                 await message.answer("An error occurred while processing your request.")
 
---
+++ translate_dict.py
@@ -0,0 +1,7 @@
+from googletrans import Translator
+
+
+def translate_text(text, source_lang, target_lang):
+    translator = Translator()
+    translation = translator.translate(text, src=source_lang, dest=target_lang)
+    return translation.text
